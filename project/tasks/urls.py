b9c8cfe26660afa1e02480a68763a8a2566dd308
martialmania <martialmania19@gmail.com>
2022-05-29 11:06:32 +0300

    Tasks api

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -1,8 +1,9 @@
 from sys import prefix
 from rest_framework import routers
-from .views import CategoryViewSet
+from .views import CategoryViewSet, TaskviewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/categories', CategoryViewSet, 'categories')
+router.register(r"api/tasks", TaskviewSet, "tasks")
 
 urlpatterns = router.urls
