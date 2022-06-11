653b313a89fb234ed266c0c691135b90fa7b9208
martialmania <martialmania19@gmail.com>
2022-06-11 06:04:57 +0300

    Adding a chart

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TaskCompletion from "src/pages/Dashboard/TaskCompletion";
+import TaskByCategory from "./TaskByCategory";
 
 export default function Dashboard() {
   return (
     <div>
       <TaskCompletion />
+      <TaskByCategory />
     </div>
   );
 }
