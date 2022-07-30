ce3848674ab31d3e31cde54ce9e2d80ec6fd6649
maniamartial <martialmania19@gmail.com>
2022-07-30 23:15:29 +0300

    Some minor comments

--- project/tasks/models.py
+++ project/tasks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 # Create your models here.
 
-
+#create a table known as category to host name, color,date
 class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
@@ -18,7 +18,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
+#create a table known as Task
 class Task(models.Model):
     class Priority(models.IntegerChoices):
         LOW = 1, "Low"
@@ -30,11 +30,14 @@ class Task(models.Model):
     description = models.TextField(max_length=1000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+
+    #this is creative, where yopu choose from the provided details
     priority = models.PositiveSmallIntegerField(
         choices=Priority.choices, default=Priority.MEDIUM)
     category = models.ForeignKey(
         Category, related_name="tasks", on_delete=models.CASCADE)
 
+#relate task to specific category, linking, fostering relationship
     created_by = models.ForeignKey(
         User, related_name="tasks", on_delete=models.CASCADE)
 
--- project/tasks/serializers.py
+++ project/tasks/serializers.py
@@ -3,15 +3,17 @@ from dataclasses import field, fields
 from rest_framework import serializers
 from django.shortcuts import render
 from .models import Category, Task
+#Srilaizer is like creating a form when you aren't using APIs
 
-
+#Allow API to use the category data
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+        #All other fields can be edited except the creator
         read_only_fields = ['created_by']
 
-
+#Allow API to acces the task table through serializers
 class TaskSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(
         read_only=True, source="category.name"
@@ -24,7 +26,7 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["created_by"]
 
-
+#Access the data that is the completed
 class DashboardTaskCompletionStatSerializer(serializers.ModelSerializer):
     count = serializers.IntegerField()
 
@@ -32,6 +34,7 @@ class DashboardTaskCompletionStatSerializer(serializers.ModelSerializer):
         model = Task
         fields = ("completed", "count")
 
+# Allow API to access the category
 class DashboardTaskCategorySerializer(serializers.ModelSerializer):
     count=serializers.IntegerField()
 
